5a45afbc7a48c45d3c80b6ed7986bd72c1362f3e
qetuop <newsgrpacc@yahoo.com>
2014-10-05 18:52:49 -0400

    new stuff

--- src/testproj/TestProj.java
+++ src/testproj/TestProj.java
@@ -28,7 +28,7 @@ public class TestProj extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Burn it all down!");
             }
         });
         
